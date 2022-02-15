ae4490675678ca97e985eed83d4a9bb58432d046
he55 <40764272+he55@users.noreply.github.com>
2022-02-15 15:46:37 +0800

    add

--- ADWpfApp1/ListBoxDragDropManager.cs
+++ ADWpfApp1/ListBoxDragDropManager.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Specialized;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
 using System.Windows.Media;
 
 namespace ADWpfApp1
@@ -10,25 +12,41 @@ namespace ADWpfApp1
     {
         ListBox listBox;
         int sel;
+
+        UIElement _originalElement;
+        SimpleCircleAdorner _overlayElement;
+        Point _startPoint;
+
         public Action<int, bool, string> DataAction;
 
         public ListBoxDragDropManager(ListBox listBox)
         {
             this.listBox = listBox;
+            this.listBox.DragEnter += ListBox_DragEnter;
             this.listBox.DragOver += ListBox_DragOver;
             this.listBox.Drop += ListBox_Drop;
+        }
+
+        private void ListBox_DragEnter(object sender, DragEventArgs e)
+        {
+            _startPoint = Mouse.GetPosition(this.listBox);
+            //DragStarted();
         }
 
         void ListBox_DragOver(object sender, DragEventArgs e)
         {
             e.Effects = DragDropEffects.Move;
             sel = GetIndexUnderDragCursor();
+
+            //DragMoved();
         }
 
         private void ListBox_Drop(object sender, DragEventArgs e)
         {
             if (lastListBoxItem != null)
                 ListBoxItemDragState.SetIsUnderDragCursor(lastListBoxItem, false);
+
+            //DragFinished();
 
             if (sel != -1)
             {
@@ -41,7 +59,7 @@ namespace ADWpfApp1
                 }
 
                 StringCollection stringCollection = data.GetFileDropList();
-                if (stringCollection.Count !=0)
+                if (stringCollection.Count != 0)
                 {
                     DataAction.Invoke(sel, true, stringCollection[0]);
                     return;
@@ -89,6 +107,29 @@ namespace ADWpfApp1
             Rect bounds = VisualTreeHelper.GetDescendantBounds(target);
             Point mousePos = MouseUtilities.GetMousePosition(target);
             return bounds.Contains(mousePos);
+        }
+
+
+
+
+        void DragFinished()
+        {
+            AdornerLayer.GetAdornerLayer(_overlayElement.AdornedElement).Remove(_overlayElement);
+        }
+
+        void DragStarted()
+        {
+            _overlayElement = new SimpleCircleAdorner(_originalElement);
+            var layer = AdornerLayer.GetAdornerLayer(_originalElement);
+            layer.Add(_overlayElement);
+        }
+
+        void DragMoved()
+        {
+            var currentPosition = Mouse.GetPosition(this.listBox);
+
+            _overlayElement.LeftOffset = currentPosition.X - _startPoint.X;
+            _overlayElement.TopOffset = currentPosition.Y - _startPoint.Y;
         }
     }
 
