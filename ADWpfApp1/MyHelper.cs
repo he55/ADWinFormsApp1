3b28656fb8d90019fb81e37282d032153e21504d
he55 <40764272+he55@users.noreply.github.com>
2022-02-16 19:56:15 +0800

    Create MyHelper.cs

---
+++ ADWpfApp1/MyHelper.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace ADWpfApp1
+{
+    public static class MyHelper
+    {
+        // How to get Item under cursor in WPF ListView
+        // https://www.py4u.net/discuss/1453642 - Answer #2
+        public static object GetObjectAtPoint( ItemsControl control, Point p)
+        {
+            var result = VisualTreeHelper.HitTest(control, p);
+            var obj = result.VisualHit;
+
+            while (VisualTreeHelper.GetParent(obj) != null && !(obj is ListBoxItem))
+            {
+                obj = VisualTreeHelper.GetParent(obj);
+            }
+
+            if (obj == null)
+                return null;
+
+            return control.ItemContainerGenerator.ItemFromContainer(obj);
+        }
+
+        public static int GetIndexAtPoint(ItemsControl control, Point p)
+        {
+            var result = VisualTreeHelper.HitTest(control, p);
+            var obj = result.VisualHit;
+
+            while (VisualTreeHelper.GetParent(obj) != null && !(obj is ListBoxItem))
+            {
+                obj = VisualTreeHelper.GetParent(obj);
+            }
+
+            if (obj == null)
+                return -1;
+
+            return control.ItemContainerGenerator.IndexFromContainer(obj);
+        }
+    }
+}
