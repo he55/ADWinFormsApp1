46192b6172f8364328bfaf7035ffb74db6fbfb9e
he55 <40764272+he55@users.noreply.github.com>
2022-10-30 16:33:54 +0800

    Update Leida.cs

--- ADNewUI/Leida.cs
+++ ADNewUI/Leida.cs
@@ -16,8 +16,12 @@ namespace ADNewUI
             double w = this.ActualWidth;
             double h = this.ActualHeight;
             dc.DrawRectangle(this.Background, null, new Rect(0, 0, w, h));
+
+            // center point
+            Point center = new Point(w / 2, h / 2);
+            dc.DrawEllipse(Brushes.Red, null, center, 2, 2);
+
             Pen pen = new Pen(Brushes.Red, 2);
-            Point center = new Point(w/2,h/2);
             double radius = w > h ? h / 2 : w / 2;
             dc.DrawEllipse(Brushes.Transparent, pen, center, radius, radius);
         }
