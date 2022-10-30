873d760ddca6c1a5c6f9d4c8996eebc3963ce8c6
he55 <40764272+he55@users.noreply.github.com>
2022-10-30 16:18:59 +0800

    add

---
+++ ADNewUI/Leida.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace ADNewUI
+{
+    public class Leida:Canvas
+    {
+        protected override void OnRender(DrawingContext dc)
+        {
+            double w = this.ActualWidth;
+            double h = this.ActualHeight;
+            dc.DrawRectangle(this.Background, null, new Rect(0, 0, w, h));
+            Pen pen = new Pen(Brushes.Red, 2);
+            Point center = new Point(w/2,h/2);
+            double radius = w > h ? h / 2 : w / 2;
+            dc.DrawEllipse(Brushes.Transparent, pen, center, radius, radius);
+        }
+    }
+}
